3e5aa368c47b1afeb8ef34ec184e21cd631a7f89
wapmorgan <wapmorgan@gmail.com>
2014-11-18 19:46:54 +0400

    Update russian plurality.

--- RussianPlurality.php
+++ RussianPlurality.php
@@ -6,11 +6,11 @@ class RussianPlurality extends BasicPlurality implements RussianCases {
 		static $plu;
 		if ($plu === null)
 			$plu = new RussianGeneralDeclension();
-		//$count = $count % 10;
-		if (in_array($count, range(2, 4))) {
-			return $plu->getForm($word, $animate, self::RODIT_2);
-		} else if ($count == 1) {
+		$ending = $count % 10;
+		if (($count > 20 && $ending == 1) || $count == 1) {
 			return $word;
+		} else if (($count > 20 && in_array($ending, range(2, 4))) || in_array($count, range(2, 4))) {
+			return $plu->getForm($word, $animate, self::RODIT_2);
 		} else {
 			$forms = $plu->pluralizeAllDeclensions($word, $animate);
 			return $forms[self::RODIT_2];
--- tests/russian_plularize_basic.phpt
+++ tests/russian_plularize_basic.phpt
@@ -4,7 +4,7 @@ test for russian pluralization.
 <?php
 require dirname(__FILE__).'/../vendor/autoload.php';
 $plu = new morphos\RussianPlurality();
-for ($i = 1; $i <= 20; $i++)
+for ($i = 1; $i <= 25; $i++)
 	echo $plu->pluralize('дом', $i)."\n";
 ?>
 --EXPECT--
@@ -27,4 +27,9 @@ for ($i = 1; $i <= 20; $i++)
 домов
 домов
 домов
+домов
+дом
+дома
+дома
+дома
 домов
