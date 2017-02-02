aa89afc844cb90fe282b4e5e94c04e001856328a
wapmorgan <wapmorgan@gmail.com>
2017-02-02 21:02:07 +0300

    Update test web script

--- web/index.php
+++ web/index.php
@@ -4,8 +4,8 @@
 </form>
 <?php
 require dirname(dirname(__FILE__)).'/vendor/autoload.php';
-$dec = new morphos\RussianGeneralDeclension();
-$plu = new morphos\RussianPlurality();
+$dec = new morphos\Russian\GeneralDeclension();
+$plu = new morphos\Russian\Plurality();
 if (isset($_POST['word'])) {
 	if (!isset($_POST['animate'])) $_POST['animate'] = false;
 	else $_POST['animate'] = true;
@@ -31,11 +31,10 @@ if (isset($_POST['word'])) {
 Введите Имя: <input name="name" value="<?= isset($_POST['name']) ? $_POST['name'] : null ?>"> <label><input type="radio" name="gender" value="m" <?= !isset($_POST['gender']) || $_POST['gender'] != 'w' ? "checked='checked'" : null ?> /> Муж. </label> <label><input type="radio" name="gender" value="w" <?= isset($_POST['gender']) && $_POST['gender'] == 'w' ? "checked='checked'" : null ?> /> Жен. </label><input type="submit"/>
 </form>
 <?php
-$dec = new morphos\RussianNamesDeclension();
 if (isset($_POST['name'])) {
-	$gender = isset($_POST['gender']) && $_POST['gender'] == 'w' ? morphos\RussianNamesDeclension::WOMAN : morphos\RussianNamesDeclension::MAN;
+	$gender = isset($_POST['gender']) && $_POST['gender'] == 'w' ? morphos\NamesDeclension::WOMAN : morphos\NamesDeclension::MAN;
 	echo '<pre>';
-		var_dump($dec->getForms($_POST['name'], $gender));
+		var_dump(morphos\Russian\nameCase($_POST['name'], $gender));
 	echo '</pre>';
 }
 ?>
