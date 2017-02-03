f34effd0c9d0ec2d99613aee66d3f3471081c06a
wapmorgan <wapmorgan@gmail.com>
2017-02-03 14:42:22 +0300

    Fix middle names generator and add tests

--- src/Russian/MiddleNamesDeclension.php
+++ src/Russian/MiddleNamesDeclension.php
@@ -7,6 +7,16 @@ namespace morphos\Russian;
 class MiddleNamesDeclension extends \morphos\NamesDeclension implements Cases {
     use RussianLanguage;
 
+    public function detectGender($name) {
+        $name = lower($name);
+        if (slice($name, -2) == 'ич')
+            return self::MAN;
+        else if (slice($name, -2) == 'на')
+            return self::WOMAN;
+
+        return null;
+    }
+
     public function getForm($name, $form, $gender) {
         $forms = $this->getForms($name, $gender);
         return $forms[$form];
@@ -26,14 +36,14 @@ class MiddleNamesDeclension extends \morphos\NamesDeclension implements Cases {
                 Cases::PREDLOJ => $this->choosePrepositionByFirstLetter($name, 'об', 'о').' '.$name.'е',
             );
         } else if (slice($name, -2) == 'на') {
-            $prefix = name(slice($name, -1));
+            $prefix = name(slice($name, 0, -1));
             return array(
                 Cases::IMENIT => $prefix.'а',
                 Cases::RODIT => $prefix.'ы',
                 Cases::DAT => $prefix.'е',
                 Cases::VINIT => $prefix.'у',
                 Cases::TVORIT => $prefix.'ой',
-                Cases::PREDLOJ => $this->choosePrepositionByFirstLetter($prefix, 'об', 'о').' '.$name.'е',
+                Cases::PREDLOJ => $this->choosePrepositionByFirstLetter($prefix, 'об', 'о').' '.$prefix.'е',
             );
         } else {
             return false;
---
+++ tests/Russian/MiddleNamesDeclensionTest.php
@@ -0,0 +1,46 @@
+<?php
+require __DIR__.'/../../vendor/autoload.php';
+
+use morphos\Russian\Cases;
+use morphos\Russian\MiddleNamesDeclension;
+use morphos\NamesDeclension;
+
+class MiddleNamesDeclensionTest extends \PHPUnit_Framework_TestCase {
+    protected $declension;
+
+    public function setUp() {
+        $this->declension = new MiddleNamesDeclension();
+    }
+
+    /**
+     * @dataProvider middleNamesProvider
+     */
+    public function testGetForms($name, $gender, $name2, $name3, $name4, $name5, $name6) {
+        $forms = $this->declension->getForms($name, $gender);
+        $this->assertEquals(array(
+            Cases::IMENIT => $name,
+            Cases::RODIT => $name2,
+            Cases::DAT => $name3,
+            Cases::VINIT => $name4,
+            Cases::TVORIT => $name5,
+            Cases::PREDLOJ => $name6
+            ), $forms);
+    }
+
+    /**
+     * @dataProvider middleNamesProvider()
+     */
+    public function testDetectGender($name, $gender) {
+        $this->assertEquals($gender, $this->declension->detectGender($name));
+    }
+
+    public function middleNamesProvider() {
+        return array(
+            array('Владимирович', NamesDeclension::MAN, 'Владимировича', 'Владимировичу', 'Владимировича', 'Владимировичем', 'о Владимировиче'),
+            array('Валерьянович', NamesDeclension::MAN, 'Валерьяновича', 'Валерьяновичу', 'Валерьяновича', 'Валерьяновичем', 'о Валерьяновиче'),
+            array('Богдановна', NamesDeclension::WOMAN, 'Богдановны', 'Богдановне', 'Богдановну', 'Богдановной', 'о Богдановне'),
+            array('Сергеевна', NamesDeclension::WOMAN, 'Сергеевны', 'Сергеевне', 'Сергеевну', 'Сергеевной', 'о Сергеевне'),
+        );
+    }
+
+}
