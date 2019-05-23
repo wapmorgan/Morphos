fdb727a38fd1d471f88e4f9a3711bb813e69956d
wapmorgan <wapmorgan@gmail.com>
2019-05-23 20:13:39 +0300

    Added tests for adjectives

---
+++ tests/Russian/AdjectivePluralizationTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace morhos\test\Russian;
+
+use morphos\Gender;
+use morphos\Russian\AdjectiveDeclension;
+use morphos\Russian\AdjectivePluralization;
+use morphos\Russian\NounDeclension;
+
+class AdjectivePluralizationTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider wordsProvider
+     *
+     * @param $word
+     * @param $animateness
+     * @param $inflected
+     */
+    public function testInflection($word, $animateness, $inflected)
+    {
+        $this->assertEquals($inflected, array_values(AdjectivePluralization::getCases($word, $animateness)));
+    }
+
+    /**
+     * @return array
+     */
+    public function wordsProvider()
+    {
+        return [
+            ['адресный', false, ['адресные', 'адресных', 'адресным', 'адресные', 'адресными', 'адресных']],
+            ['выездной', false, ['выездные', 'выездных', 'выездным', 'выездные', 'выездными', 'выездных']],
+            ['домашний', false, ['домашние', 'домашних', 'домашним', 'домашние', 'домашними', 'домашних']],
+            ['дилерский', false, ['дилерские', 'дилерских', 'дилерским', 'дилерские', 'дилерскими', 'дилерских']],
+            ['сухой', false, ['сухие', 'сухих', 'сухим', 'сухие', 'сухими', 'сухих']],
+            ['большой', false, ['большие', 'больших', 'большим', 'большие', 'большими', 'больших']],
+        ];
+    }
+}
